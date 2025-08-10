2b85bc6d0fbe4bc2d6babfa680cc3da301d44d5f
CesarLoor <ciloor2@espe.edu.ec>
2025-08-10 13:18:22 -0500

    ejecución workflow extra

---
+++ math.js
@@ -0,0 +1,16 @@
+function factorial(n) {
+    if (n < 0) throw new Error('No se permite factorial negativo');
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+  }
+  
+  function fibonacci(n) {
+    if (n < 0) throw new Error('No se permite número negativo');
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+  
+  module.exports = { factorial, fibonacci };
+
+  
